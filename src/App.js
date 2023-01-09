2ec269cb151b47367472442326c717943407ec37
github BeomJu <github gtmxhs@gmail.com>
2023-01-09 13:01:11 +0900

    1stMerge

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Footer from './component/footer/Footer';
 
 function App() {
   return (
-    <div>
-      <div className="App">
-        <Topbar></Topbar>
-        <Home></Home>
-        <Footer></Footer>
-      </div>
+    <div className="App">
+      <Topbar></Topbar>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
